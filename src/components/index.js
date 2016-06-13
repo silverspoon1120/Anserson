85c2a2827e08c41e1ef67ce1963695eb84f08906
Silver Spoon <silverspoon1120.sw@gmail.com>
2016-06-13 22:45:51 +0530

    Added the support for the webpack

--- src/components/index.js
+++ src/components/index.js
@@ -22,6 +22,5 @@ FormioComponents.signature = require('./signature');
 FormioComponents.textarea = require('./textarea');
 FormioComponents.textfield = require('./textfield');
 FormioComponents.well = require('./well');
-FormioComponents.currency = require('./currency');
 
 module.exports = {};
