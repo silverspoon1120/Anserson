bb98da995f7d23bbe14ef5a6dcf44adbccfd2e0e
Silver Spoon <silverspoon1120.sw@gmail.com>
2016-06-28 10:20:32 -0500

    Add more data to onChange event.

--- src/components/hidden.jsx
+++ src/components/hidden.jsx
@@ -1,11 +1,9 @@
 var React = require('react');
-var valueMixin = require('./mixins/valueMixin.jsx');
 
 module.exports = React.createClass({
-  mixins: [valueMixin],
   displayName: 'Hidden',
-  getElements: function() {
-    var value = (this.state && this.state.hasOwnProperty('value')) ? this.state.value : '';
+  render: function() {
+    var value = (this.props.data && this.props.data.hasOwnProperty(this.props.component.key)) ? this.props.data[this.props.component.key] : '';
     return (
       <input type="hidden" id={this.props.component.key} name={this.props.component.key} value={value}></input>
     );
