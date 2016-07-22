82ccce579357fb59fa259fc8b0c7a5172cff5986
Silver Spoon <silverspoon1120.sw@gmail.com>
2016-07-22 18:34:10 +0530

    Testcase for the Textarea component .

--- test/textarea-test.js
+++ test/textarea-test.js
@@ -2,71 +2,74 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
-import Email from '../src/components/email.jsx';
+import Textarea from '../src/components/textarea.jsx';
 import sinon from 'sinon';
-
 import form from './forms/empty.json';
 
-describe('Email', function () {
-  describe('Single email', function () {
+describe('Textarea', function () {
+  describe('Single Textarea', function () {
     var component= {
-      "validate": {
-        "required": false
-      },
-      "conditional": {
-        "eq": "",
-        "when": null,
-        "show": ""
-      },
-      "type": "email",
-      "persistent": true,
-      "unique": true,
-      "protected": false,
-      "defaultValue": "",
-      "suffix": "",
-      "prefix": "",
-      "placeholder": "",
-      "key": "email",
-      "label": "my email",
-      "inputType": "email",
+      "input": true,
       "tableView": true,
-      "input": true
+      "label": "textarea",
+      "key": "textarea",
+      "placeholder": "",
+      "prefix": "",
+      "suffix": "",
+      "rows": 3,
+      "multiple": false,
+      "defaultValue": "",
+      "protected": false,
+      "persistent": true,
+      "wysiwyg": false,
+      "validate": {
+        "required": false,
+        "minLength": "",
+        "maxLength": "",
+        "pattern": "",
+        "custom": ""
+      },
+      "type": "textarea",
+      "conditional": {
+        "show": "",
+        "when": null,
+        "eq": ""
+      }
     };
-
     var attachToForm = sinon.spy();
-    it('Renders a basic email', function (done) {
+
+    it('Renders a basic textarea', function (done) {
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       ).children().eq(0);
       expect(element).to.have.length(1);
-      expect(element.hasClass('form-group form-field-type-email form-group-email')).to.equal(true);
-      expect(element.attr('id')).to.equal('form-group-email');
+      expect(element.hasClass('form-group form-field-type-textarea form-group-textarea')).to.equal(true);
+      expect(element.attr('id')).to.equal('form-group-textarea');
       expect(element.find('.formio-component-single').length).to.equal(1);
       expect(element.find('.formio-component-single label').length).to.equal(1);
-      expect(element.find('.formio-component-single label').html()).to.equal('my email');
-      expect(element.find('.formio-component-single label').attr('for')).to.equal('email');
+      expect(element.find('.formio-component-single label').html()).to.equal('textarea');
+      expect(element.find('.formio-component-single label').attr('for')).to.equal('textarea');
       expect(element.find('.formio-component-single .input-group').length).to.equal(1);
-      expect(element.find('.formio-component-single .input-group input').length).to.equal(1);
-      expect(element.find('.formio-component-single .input-group input').attr('class')).to.equal('form-control');
-      expect(element.find('.formio-component-single .input-group input').attr('id')).to.equal('email');
-      expect(element.find('.formio-component-single .input-group input').attr('data-index')).to.equal('0');
-      expect(element.find('.formio-component-single .input-group input').attr('value')).to.equal('');
-      expect(element.find('.formio-component-single .input-group input').attr('placeholder')).to.equal('');
+      expect(element.find('.formio-component-single .input-group textarea').length).to.equal(1);
+      expect(element.find('.formio-component-single .input-group textarea').attr('class')).to.equal('form-control');
+      expect(element.find('.formio-component-single .input-group textarea').attr('id')).to.equal('textarea');
+      expect(element.find('.formio-component-single .input-group textarea').attr('data-index')).to.equal('0');
+      expect(element.find('.formio-component-single .input-group textarea').attr('placeholder')).to.equal('');
       done();
     });
 
     it('Fills in the placeholder value', function(done) {
-      component.placeholder = 'Test Placeholder';
+      component.placeholder = 'My Placeholder';
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
-      ).find('input#email');
-      expect(element.attr('placeholder')).to.equal('Test Placeholder');
+        ></Textarea>
+      ).find('textarea');
+      expect(element.attr('placeholder')).to.equal('My Placeholder');
       component.placeholder = '';
       done();
     });
@@ -74,10 +77,10 @@ describe('Email', function () {
     it('Renders with a prefix', function(done) {
       component.prefix = '$';
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       ).find('.input-group');
       expect(element.children().length).to.equal(2);
       expect(element.children().eq(0).hasClass('input-group-addon')).to.equal(true);
@@ -89,10 +92,10 @@ describe('Email', function () {
     it('Renders with a suffix', function(done) {
       component.suffix = 'Pounds';
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       ).find('.input-group');
       expect(element.children().length).to.equal(2);
       expect(element.children().eq(1).hasClass('input-group-addon')).to.equal(true);
@@ -105,10 +108,10 @@ describe('Email', function () {
       component.prefix = 'Prefix';
       component.suffix = 'Suffix';
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       ).find('.input-group');
       expect(element.children().length).to.equal(3);
       expect(element.children().eq(0).hasClass('input-group-addon')).to.equal(true);
@@ -120,49 +123,37 @@ describe('Email', function () {
       done();
     });
 
-    it('Sets a default value', function(done) {
-      const element = render(
-        <Email
-      component={component}
-      value='testEmail@test.com'
-      attachToForm={attachToForm}
-        ></Email>
-      ).find('input');
-      expect(element.attr('value')).to.equal('testEmail@test.com');
-      done();
-    });
-
-    it('Check single email with required', function(done) {
+    it('Check single textarea with required', function(done) {
       component.validate.required = true;
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       );
       expect(element.find('.formio-component-single label ').attr('class')).to.equal('control-label field-required');
       done();
     });
 
-    it('Check single email without required', function(done) {
+    it('Check single textarea without required', function(done) {
       component.validate.required = false;
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       );
       expect(element.find('.formio-component-single label ').attr('class')).to.equal('control-label');
       done();
     });
 
-    it('Check single email without label', function(done) {
+    it('Check single textarea without label', function(done) {
       component.label = '';
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       );
       expect(element.find('.formio-component-single label').length).to.equal(0);
       done();
@@ -170,69 +161,74 @@ describe('Email', function () {
 
   });
 
-  describe('Multiple email', function() {
+  describe('Multiple Textarea', function() {
     var component = {
-      "multiple": true,
-      "validate": {
-        "required": true
-      },
-      "conditional": {
-        "eq": "",
-        "when": null,
-        "show": ""
-      },
-      "type": "email",
-      "persistent": true,
-      "unique": true,
-      "protected": false,
-      "defaultValue": "",
-      "suffix": "",
-      "prefix": "",
-      "placeholder": "",
-      "key": "email",
-      "label": "my email",
-      "inputType": "email",
+      "input": true,
       "tableView": true,
-      "input": true
+      "label": "textarea",
+      "key": "textarea",
+      "placeholder": "",
+      "prefix": "",
+      "suffix": "",
+      "rows": 3,
+      "multiple": true,
+      "defaultValue": "",
+      "protected": false,
+      "persistent": true,
+      "wysiwyg": false,
+      "validate": {
+        "required": false,
+        "minLength": "",
+        "maxLength": "",
+        "pattern": "",
+        "custom": ""
+      },
+      "type": "textarea",
+      "conditional": {
+        "show": "",
+        "when": null,
+        "eq": ""
+      }
     };
     var attachToForm = sinon.spy();
-    it('Renders a multi-value email', function(done) {
+
+    it('Renders a multi-value textarea', function(done) {
       const element = render(
-        <Email
-      name="email"
+        <Textarea
+      name="textarea"
       component={component}
       attachToForm={attachToForm}
-        ></Email>
-      ).find('.form-field-type-email');
+        ></Textarea>
+      ).find('.form-field-type-textarea');
       expect(element).to.have.length(1);
-      expect(element.hasClass('form-group form-field-type-email form-group-email')).to.equal(true);
-      expect(element.attr('id')).to.equal('form-group-email');
+      expect(element.hasClass('form-group form-field-type-textarea form-group-textarea')).to.equal(true);
+      expect(element.attr('id')).to.equal('form-group-textarea');
       expect(element.children().eq(0).hasClass('formio-component-multiple')).to.equal(true);
-      expect(element.children().eq(0).children().eq(0).attr('for')).to.equal('email');
+      expect(element.children().eq(0).children().eq(0).attr('for')).to.equal('textarea');
       expect(element.children().eq(0).children().eq(0).hasClass('control-label')).to.equal(true);
-      expect(element.children().eq(0).children().eq(0).text()).to.equal('my email');
+      expect(element.children().eq(0).children().eq(0).text()).to.equal('textarea');
       const table = element.children().eq(0).children().eq(1);
       expect(table.hasClass('table table-bordered')).to.equal(true);
       expect(table.find('tr').length).to.equal(2);
       expect(table.find('tr td div.input-group').length).to.equal(1);
-      expect(table.find('tr td div.input-group input').attr('placeholder')).to.equal('');
-      expect(table.find('tr td div.input-group input').attr('value')).to.equal('');
-      expect(table.find('tr td div.input-group input').attr('id')).to.equal('email');
-      expect(table.find('tr td div.input-group input').attr('name')).to.equal('email');
-      expect(table.find('tr td div.input-group input').attr('class')).to.equal('form-control');
-      expect(table.find('tr td div.input-group input').attr('data-index')).to.equal('0');
+      expect(table.find('tr td div.input-group textarea').attr('placeholder')).to.equal('');
+      //expect(table.find('tr td div.input-group textarea').attr('value')).to.equal('');
+      expect(table.find('tr td div.input-group textarea').attr('id')).to.equal('textarea');
+      expect(table.find('tr td div.input-group textarea').attr('name')).to.equal('textarea');
+      expect(table.find('tr td div.input-group textarea').attr('class')).to.equal('form-control');
+      expect(table.find('tr td div.input-group textarea').attr('data-index')).to.equal('0');
       done();
     });
 
     it('Fills in the placeholder value', function(done) {
       component.placeholder = 'My Placeholder';
       const element = render(
-        <Email
-      name="email"
+        <Textarea
+      name="textarea"
       component={component}
       attachToForm={attachToForm}
-        ></Email>
-      ).find('input#email');
+        ></Textarea>
+      ).find('textarea#textarea');
       expect(element.attr('placeholder')).to.equal('My Placeholder');
       component.placeholder = '';
       done();
@@ -241,11 +237,11 @@ describe('Email', function () {
     it('Renders with a prefix', function(done) {
       component.prefix = '$';
       const element = render(
-        <Email
-      name="email"
+        <Textarea
+      name="textarea"
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       ).find('.input-group');
       expect(element.children().length).to.equal(2);
       expect(element.children().eq(0).hasClass('input-group-addon')).to.equal(true);
@@ -257,11 +253,11 @@ describe('Email', function () {
     it('Renders with a suffix', function(done) {
       component.suffix = 'Pounds';
       const element = render(
-        <Email
-      name="email"
+        <Textarea
+      name="textarea"
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       ).find('.input-group');
       expect(element.children().length).to.equal(2);
       expect(element.children().eq(1).hasClass('input-group-addon')).to.equal(true);
@@ -274,11 +270,11 @@ describe('Email', function () {
       component.prefix = 'Prefix';
       component.suffix = 'Suffix';
       const element = render(
-        <Email
-      name="email"
+        <Textarea
+      name="textarea"
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       ).find('.input-group');
       expect(element.children().length).to.equal(3);
       expect(element.children().eq(0).hasClass('input-group-addon')).to.equal(true);
@@ -291,81 +287,81 @@ describe('Email', function () {
     });
 
     it('Adds and removes rows', function(done) {
+      component.defaultValue = 'My Value';
       const element = mount(
-        <Email
-      name="email"
+        <Textarea
+      name="textarea"
       component={component}
       attachToForm={attachToForm}
-        ></Email>
-      ).find('.form-field-type-email');
+        ></Textarea>
+      ).find('.form-field-type-textarea');
       const table = element.find('table');
       table.find('a.btn.add-row').simulate('click');
       expect(table.find('tr').length).to.equal(3);
-      expect(table.find('tr').at(0).find('input').prop('data-index')).to.equal(0);
-      expect(table.find('tr').at(1).find('input').prop('data-index')).to.equal(1);
+      expect(table.find('tr').at(0).find('textarea').prop('data-index')).to.equal(0);
+      expect(table.find('tr').at(1).find('textarea').prop('data-index')).to.equal(1);
       table.find('a.btn.add-row').simulate('click');
       expect(table.find('tr').length).to.equal(4);
-      expect(table.find('tr').at(0).find('input').prop('data-index')).to.equal(0);
-      expect(table.find('tr').at(1).find('input').prop('data-index')).to.equal(1);
-      expect(table.find('tr').at(2).find('input').prop('data-index')).to.equal(2);
+      expect(table.find('tr').at(0).find('textarea').prop('data-index')).to.equal(0);
+      expect(table.find('tr').at(1).find('textarea').prop('data-index')).to.equal(1);
+      expect(table.find('tr').at(2).find('textarea').prop('data-index')).to.equal(2);
       table.find('a.btn.add-row').simulate('click');
       expect(table.find('tr').length).to.equal(5);
-      expect(table.find('tr').at(0).find('input').prop('data-index')).to.equal(0);
-      expect(table.find('tr').at(1).find('input').prop('data-index')).to.equal(1);
-      expect(table.find('tr').at(2).find('input').prop('data-index')).to.equal(2);
-      expect(table.find('tr').at(3).find('input').prop('data-index')).to.equal(3);
+      expect(table.find('tr').at(0).find('textarea').prop('data-index')).to.equal(0);
+      expect(table.find('tr').at(1).find('textarea').prop('data-index')).to.equal(1);
+      expect(table.find('tr').at(2).find('textarea').prop('data-index')).to.equal(2);
+      expect(table.find('tr').at(3).find('textarea').prop('data-index')).to.equal(3);
       table.find('a.btn.remove-row-3').simulate('click');
       expect(table.find('tr').length).to.equal(4);
-      expect(table.find('tr').at(0).find('input').prop('data-index')).to.equal(0);
-      expect(table.find('tr').at(1).find('input').prop('data-index')).to.equal(1);
-      expect(table.find('tr').at(2).find('input').prop('data-index')).to.equal(2);
+      expect(table.find('tr').at(0).find('textarea').prop('data-index')).to.equal(0);
+      expect(table.find('tr').at(1).find('textarea').prop('data-index')).to.equal(1);
+      expect(table.find('tr').at(2).find('textarea').prop('data-index')).to.equal(2);
       table.find('a.btn.remove-row-1').simulate('click');
       expect(table.find('tr').length).to.equal(3);
-      expect(table.find('tr').at(0).find('input').prop('data-index')).to.equal(0);
-      expect(table.find('tr').at(1).find('input').prop('data-index')).to.equal(1);
+      expect(table.find('tr').at(0).find('textarea').prop('data-index')).to.equal(0);
+      expect(table.find('tr').at(1).find('textarea').prop('data-index')).to.equal(1);
       table.find('a.btn.remove-row-1').simulate('click');
       expect(table.find('tr').length).to.equal(2);
-      expect(table.find('tr').at(0).find('input').prop('data-index')).to.equal(0);
+      expect(table.find('tr').at(0).find('textarea').prop('data-index')).to.equal(0);
       table.find('a.btn.remove-row-0').simulate('click');
       expect(table.find('tr').length).to.equal(1);
       table.find('a.btn.add-row').simulate('click');
       expect(table.find('tr').length).to.equal(2);
-      expect(table.find('tr').at(0).find('input').prop('data-index')).to.equal(0);
+      expect(table.find('tr').at(0).find('textarea').prop('data-index')).to.equal(0);
       done();
     })
 
-    it('Check multiple email with required', function(done) {
+    it('Check multiple textarea with required', function(done) {
       component.validate.required = true;
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       );
       expect(element.find('.formio-component-multiple label ').attr('class')).to.equal('control-label field-required');
       done();
     });
 
-    it('Check multiple email without required', function(done) {
+    it('Check multiple textarea without required', function(done) {
       component.validate.required = false;
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       );
       expect(element.find('.formio-component-multiple label ').attr('class')).to.equal('control-label');
-      component.validate.required = true;
       done();
     });
 
-    it('Check multiple email without label', function(done) {
+    it('Check multiple textarea without label', function(done) {
       component.label = '';
       const element = render(
-        <Email
+        <Textarea
       component={component}
       attachToForm={attachToForm}
-        ></Email>
+        ></Textarea>
       );
       expect(element.find('.formio-component-multiple label').length).to.equal(0);
       done();
@@ -373,4 +369,3 @@ describe('Email', function () {
   });
 
 });
-
